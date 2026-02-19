db6a1d8c29d6a041c9601f8a9f6667d8d568291d
Dorin Tihon <dorinmac@Dorins-MacBook-Air.local>
2026-02-19 22:39:11 +0200

    Designed navigation bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 
 import { Link } from "react-router-dom";
+import styles from '../styles/Home.module.css'
 
 
 function Home() {
     return (
         <>
-            <main>
-                <h1>Welcome to the Shopping Cart</h1>
-                <Link to="/shop">Go to Shop</Link>
+            <main className={styles.main}>
+                <h1 className={styles.title}>Welcome to the Shopping Cart</h1>
+                <Link to="/shop" className={styles.button}>Go to Shop</Link>
             </main>
         </>
     )
